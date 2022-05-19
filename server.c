78b9a4267eda424c7d058ceda3193b3fe4232c45
James Amanatidis <jamanatidis@student.unimelb.edu.au>
2022-05-19 19:49:57 +1000

    Attempted (and failed) proper path component checking. Lots of comments and optimizations.

--- server.c
+++ server.c
@@ -1,9 +1,18 @@
+/*
+
+server.c
+
+A basic web server that handles HTTP 1.0 GET requests and responds appropiately.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
 #include <ctype.h>
+#include <limits.h>
 #include <pthread.h>
 #include <sys/stat.h>
 
@@ -12,38 +21,46 @@
 
 #define N_THREADS 10
 
-#define GET_PATH_START 4 // The index of a request where the pathname begins.
+#define LONGEST_MIME_TYPE 25 	// The longest MIME type we will put in the header.
+#define INITIAL_PATH_SIZE 100 	// The initial size of the path buffer.
+#define INITIAL_OUTPUT_SIZE 200 // The initial size of the outgoing buffer.
+#define MAX_REQUEST_SIZE 2000 	// The largest GET request we can expect.
+#define MAX_SYN_PACKETS 10	
 
-#define LONGEST_MIME_TYPE 25 // The longest MIME type we will put in the header.
-#define INITIAL_OUTPUT_SIZE 500 // The initial size of the outgoing buffer.
-#define MAX_REQUEST_SIZE 2000 // The largest GET request we can expect.
-#define MAX_SYN_PACKETS 10
+#define PROTOCOL_ARG 1			// The argv index for IPv4/IPv6.
+#define PORT_ARG 2				// The argv index for the port to use.
+#define PATH_ARG 3				// The argv index for the web root path.
 
-#define PROTOCOL_ARG 1
-#define PORT_ARG 2
-#define PATH_ARG 3
-
+// A structure for storing parameters to pass to threads.
 typedef struct thread_info {
 	int connfd;
 	char *web_root;
 } thread_info_t;
 
+// Callable function for threads to communicate on a connection socket.
 void *handle_connection(void *p) {
 
+	// Tidy up the parameters passed.
 	thread_info_t *t_info = (thread_info_t *) p;
 	int n, connfd = t_info->connfd;
 	char *web_root = t_info->web_root;
-	printf("Started thread handling socket %d\n", connfd);
 	
+	printf("Started thread handling socket %d\n", connfd);
+
+	// Prepare buffers.
 	char i_buffer[MAX_REQUEST_SIZE + 1];
-	char path_buffer[MAX_REQUEST_SIZE * 2];
-	int o_buffer_n = INITIAL_OUTPUT_SIZE;
+	
+	size_t o_buffer_n = INITIAL_OUTPUT_SIZE;
 	char *o_buffer = malloc(INITIAL_OUTPUT_SIZE * sizeof(*o_buffer));
+	if (o_buffer == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
 	// Start a pleasant conversation.
 	memset(i_buffer, 0, sizeof(i_buffer));
-	int is_finished = 0, bytes_read = 0;
-	while (!is_finished) {
+	int bytes_read = 0;
+	while (1) {
 		// Read into the buffer, shifted over accordingly.
 		n = recv(connfd, i_buffer + bytes_read, MAX_REQUEST_SIZE - bytes_read, 0);
 		if (n < 0) {
@@ -52,145 +69,176 @@ void *handle_connection(void *p) {
 		}
 		bytes_read += n;
 		
-		// If there's two new-lines in a row, the HTTP message is complete.
-		if (strstr(i_buffer, "\n\n\n\n") != NULL || strstr(i_buffer, "\r\n\r\n") != NULL) {
-			is_finished = 1;
+		// If there's two new-lines in a row, or the client disconnected, the HTTP message is complete.
+		if (n == 0 || strstr(i_buffer, "\n\n\n\n") != NULL || strstr(i_buffer, "\r\n\r\n") != NULL) {
+			break;
 		}
 	}
-	printf("==MESSAGE==\n%s\n==END==\nBytes read: %d\n", i_buffer, bytes_read);
 	
-	// An awfully direct way of checking that this is a GET request;
-	// if it starts with the 5 characters "GET /" (with any casing), it's valid!
-	int valid_request = 0;
-	if (bytes_read > 4
-		  && tolower(i_buffer[0]) 	== 	'g' 
-		  && tolower(i_buffer[1]) 	== 	'e' 
-		  && tolower(i_buffer[2]) 	== 	't' 
-		  && i_buffer[3] 			== 	' '
-		  && i_buffer[4] 			== 	'/') {
-		valid_request = 1;
-	}
-
-	// Time to prepare a response.
-	if (!valid_request) {
-		printf("Bad syntax. Returning 400.\n");
-		snprintf(o_buffer, o_buffer_n, "HTTP/1.0 400 Bad Request\r\n\r\n");
-		o_buffer_n = strlen(o_buffer);
+	if (n == 0) {
+		printf("Client disconnected unexpectedly.\n");
 	} else {
-		// Get the length of the requested path.
-		int len = 0;
-		for (int i = 4; i < bytes_read; i++) {
-			// Check if we've reached the end of the path.
-			if (i_buffer[i] == ' ') {
-				break;
-			}
-			
-			// Look for any invalid path components while we're here.
-			if (len > 1
-				  && i_buffer[i] 	 == '/'
-				  && i_buffer[i - 1] == '.'
-				  && i_buffer[i - 2] == '.') {
-				valid_request = 0;
-				break;
-			}
-			
-			len++;
-		}
-		// Concatenate the root directory and requested path.
-		strcpy(path_buffer, web_root);
-		strncat(path_buffer, i_buffer + 4, len);
-		printf("Requested: '%s'\n", path_buffer);
+		printf("==MESSAGE==\n%s\n==END==\nBytes read: %d\n", i_buffer, bytes_read);
 		
-		// Check the path.
-		struct stat stat_buffer;
-		if (valid_request && stat(path_buffer, &stat_buffer) == 0 && S_ISREG(stat_buffer.st_mode)) {
-			printf("Found! Returning 200.\n");
+		// Time to prepare a response.
+		// An awfully direct way of checking that this is a GET request;
+		// if it starts with the 5 characters "GET /" (with any casing), it's valid!
+		if (!(bytes_read > 4
+			  && tolower(i_buffer[0]) 	== 	'g' 
+			  && tolower(i_buffer[1]) 	== 	'e' 
+			  && tolower(i_buffer[2]) 	== 	't' 
+			  && i_buffer[3] 			== 	' '
+			  && i_buffer[4] 			== 	'/')) {
+			printf("Bad syntax. Returning 400.\n");
+			snprintf(o_buffer, o_buffer_n, "HTTP/1.0 400 Bad Request\r\n\r\n");
+			o_buffer_n = strlen(o_buffer);
+		} else {
 			
-			// Open the requested file.
-			FILE *fp = fopen(path_buffer, "rb");
-			if (fp == NULL) {
-				perror("fopen");
+			// Get the requested path and copy it.
+			char request_path_buffer[MAX_REQUEST_SIZE];
+			for (int i = 4; i < bytes_read; i++) {
+				// Check if we've reached the end of the path.
+				if (i_buffer[i] == ' ') {
+					// Null terminate and exit loop.
+					request_path_buffer[i - 4] = 0;
+					break;
+				}
+				request_path_buffer[i - 4] = i_buffer[i];
+			}
+			
+			// Concatenate the canonical root with the requested path.
+			char *full_path = malloc((strlen(web_root) + MAX_REQUEST_SIZE + 1) * sizeof(*full_path));
+			if (full_path == NULL) {
+				perror("malloc");
 				exit(EXIT_FAILURE);
 			}
 			
-			int ext_start = strlen(path_buffer);
-			for (int i = ext_start; i > 0; i--) {
-				if (path_buffer[i] == '.') {
-					ext_start = i;
-					break;
+			strcpy(full_path, web_root);
+			strcat(full_path, request_path_buffer);
+			
+			// Now get the overall canonical path.
+			char *full_real_path = realpath(full_path, NULL);
+			if (full_real_path == NULL) {
+				perror("realpath");
+				exit(EXIT_FAILURE);
+			}			
+			
+			printf("Requested: '%s'\n", full_real_path);
+			
+			// Check the path, ensuring it's within the web root directory,
+			// that it exists, and that it is a normal readable file.
+			struct stat stat_buffer;
+			if (strncmp(full_real_path, web_root, strlen(web_root)) == 0
+				  && stat(full_real_path, &stat_buffer) == 0 
+				  && S_ISREG(stat_buffer.st_mode)) {
+				
+				// Open the requested file.
+				FILE *fp = fopen(full_real_path, "rb");
+				if (fp == NULL) {
+					perror("fopen");
+					exit(EXIT_FAILURE);
 				}
-			}
-			
-			// Get the corresponding MIME type.
-			char mime_type[LONGEST_MIME_TYPE];
-			if (strcmp(path_buffer + ext_start, ".html") == 0) {
-				strcpy(mime_type, "text/html");
-			} else if (strcmp(path_buffer + ext_start, ".jpg") == 0) {
-				strcpy(mime_type, "image/jpeg");
-			} else if (strcmp(path_buffer + ext_start, ".css") == 0) {
-				strcpy(mime_type, "text/css");
-			} else if (strcmp(path_buffer + ext_start, ".js") == 0) {
-				strcpy(mime_type, "text/javascript");
+				
+				// Look for the last dot in the filename.
+				int ext_start = strlen(full_real_path);
+				for (int i = ext_start; i > 0; i--) {
+					if (full_real_path[i] == '.') {
+						ext_start = i;
+						break;
+					}
+				}
+				
+				// Get the corresponding MIME type.
+				char mime_type[LONGEST_MIME_TYPE];
+				if (strcmp(full_real_path + ext_start, ".html") == 0) {
+					strcpy(mime_type, "text/html");
+				} else if (strcmp(full_real_path + ext_start, ".jpg") == 0) {
+					strcpy(mime_type, "image/jpeg");
+				} else if (strcmp(full_real_path + ext_start, ".css") == 0) {
+					strcpy(mime_type, "text/css");
+				} else if (strcmp(full_real_path + ext_start, ".js") == 0) {
+					strcpy(mime_type, "text/javascript");
+				} else {
+					strcpy(mime_type, "application/octet-stream");
+				}
+				
+				// Construct the header.
+				printf("Found! Returning 200.\n");
+				snprintf(o_buffer, o_buffer_n, 
+					  "HTTP/1.0 200 OK\r\nContent-Length: %ld\r\nContent-Type: %s\r\n\r\n", 
+					  stat_buffer.st_size, mime_type);
+				
+				// "Append" the file contents.
+				int header_length = strlen(o_buffer);
+				o_buffer_n = header_length + stat_buffer.st_size;
+				o_buffer = realloc(o_buffer, o_buffer_n * sizeof(*o_buffer));
+				if (o_buffer == NULL) {
+					perror("realloc");
+					exit(EXIT_FAILURE);
+				}
+				
+				fread(o_buffer + header_length, o_buffer_n, 1, fp);
+				
+				fclose(fp);				
 			} else {
-				strcpy(mime_type, "application/octet-stream");
+				printf("Not found. Returning 404.\n");
+				snprintf(o_buffer, o_buffer_n, "HTTP/1.0 404 Not Found\r\n\r\n");
+				o_buffer_n = strlen(o_buffer);
 			}
 			
-			// Construct the header.
-			snprintf(o_buffer, o_buffer_n, 
-				  "HTTP/1.0 200 OK\r\nContent-Length: %ld\r\nContent-Type: %s\r\n\r\n", 
-				  stat_buffer.st_size, mime_type);
-			
-			// "Append" the file contents.
-			int header_length = strlen(o_buffer);
-			o_buffer_n = header_length + stat_buffer.st_size;
-			o_buffer = realloc(o_buffer, o_buffer_n * sizeof(*o_buffer));
-			fread(o_buffer + header_length, o_buffer_n, 1, fp);
-			fclose(fp);				
-		} else {
-			printf("Not found. Returning 404.\n");
-			snprintf(o_buffer, o_buffer_n, "HTTP/1.0 404 Not Found\r\n\r\n");
-			o_buffer_n = strlen(o_buffer);
-		}
-	}		
+			free(full_path);
+			free(full_real_path);
+		}		
+		
+		// Send our message to the client.
+		n = send(connfd, o_buffer, o_buffer_n, 0);
+		if (n < 0) {
+			perror("send");
+			exit(EXIT_FAILURE);
+		}	
+	}	
 	
-	// Send our message to the client.
-	n = send(connfd, o_buffer, o_buffer_n, 0);
-	if (n < 0) {
-		perror("send");
-		exit(EXIT_FAILURE);
-	}		
-	
-	// Close the connection.
+	// Close the connection and free memory.
 	free(o_buffer);
 	free(t_info);
 	close(connfd);
 	return NULL;
 }
+
 /* Basic TCP socket creation and connection code adapted from
 various COMP30023 materials - namely, server.c from Practical 9
 and Week 8 lecture notes. Credit to Ayesha Ahmed and Lachlan Andrew. */
 
 int main(int argc, char **argv) {
 	
+	// Very basic input checking.
 	if (argc != 4) {
 		perror("Invalid number of arguments!");
 		exit(EXIT_FAILURE);
 	}
+
+	// Get the canonical file path of the web root.
+	char *real_root = realpath(argv[PATH_ARG], NULL);
+	if (real_root == NULL) {
+		perror("realpath");
+		exit(EXIT_FAILURE);
+	}	
 	
+	// Initialize variables.
 	pthread_t tid[N_THREADS];
 	thread_info_t *t_info[N_THREADS];
 	int thread_n = 0;
 	
 	int listenfd = 0, re = 1, s;
-
 	struct addrinfo hints, *res, *rp;
 	
+	// Determine whether to use IPv4 or IPv6.
 	int is_ipv4 = 1;
 	if (strcmp(argv[PROTOCOL_ARG], "6") == 0) {
 		is_ipv4 = 0;
 	}
 
-	// Set up our connection - IPv4, TCP, and passive.
+	// Set up our connection.
 	memset(&hints, 0, sizeof(hints));
 	if (is_ipv4) {
 		hints.ai_family = AF_INET;
@@ -239,22 +287,25 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 	
-	struct sockaddr_storage client_addr;
-	socklen_t client_addr_size = sizeof(client_addr);	
-	
 	while (1) {
 		// Wait for an incoming connection and capture the remote address.
-		int connfd = accept(listenfd, (struct sockaddr *) &client_addr, &client_addr_size);
+		int connfd = accept(listenfd, NULL, NULL);
 		if (connfd < 0) {
 			perror("accept");
 			exit(EXIT_FAILURE);
 		}
-		
-		// Create a new thread to handle incoming connection.
+
+		// Initialize parameters to pass to a new thread.
 		t_info[thread_n] = malloc(sizeof(t_info));
-		t_info[thread_n]->connfd = connfd;
-		t_info[thread_n]->web_root = argv[PATH_ARG];
+		if (t_info[thread_n] == NULL) {
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
 		
+		t_info[thread_n]->connfd = connfd;
+		t_info[thread_n]->web_root = real_root;
+		
+		// Create a new detached thread to handle this connection.
 		if (pthread_create(&tid[thread_n], NULL, handle_connection, t_info[thread_n])) {
 			perror("pthread_create");
 			exit(EXIT_FAILURE);
@@ -264,17 +315,18 @@ int main(int argc, char **argv) {
 			perror("pthread_detach");
 			exit(EXIT_FAILURE);
 		}
-		thread_n++;
 		
 		// Cycle through the threads. This will probably have cataclysmic
 		// consequences if more than N_THREADS threads run at a time, but maybe
 		// not since each thread is detached anyway. I don't know.
 		// Multi-threading is hard :(
+		thread_n++;
 		if (thread_n >= N_THREADS) {
 			thread_n = 0;
 		}
 	}
 
+	free(real_root);
 	close(listenfd);
 	return 0;
 }
